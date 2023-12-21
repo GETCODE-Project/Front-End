2aab70f2d79441914bff37609ea124547f7da209
김한빈 <gimhanbin@gimhanbin-ui-MacBookPro.local>
2023-12-22 00:02:11 +0900

    Init: 초기환경세팅(next,style-components)

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from 'next/app';
+import {ThemeProvider} from 'styled-components';
+import { GlobalStyle } from '@/styles/global-style';
+import { theme } from '@/styles/theme';
+import '@/styles/globals.css';
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
