4d899a45234773ae1a1631751cd5677054d2c0d3
juyeon <oh_ju_yeon@naver.com>
2023-12-28 20:21:01 +0900

    Revert "Feat: RoundBox, Toggle, ProjectForm 컴포넌트 생성"
    This reverts commit 3c75d5824647625a42bef97e2079749a76235c53.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,10 @@
-import styles from "@/styles/Home.module.css";
-import "styled-components";
-import styled from "styled-components";
+import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
-      <h1>TEST CODE</h1>
-      <Div>dd</Div>
+     <h1>Hello World!</h1>
+     <h1>TEST CODE</h1>
     </>
-  );
+  )
 }
-
-const Div = styled.div`
-  color: red;
-  font-size: 2rem;
-`;
--- pages/project/detail/index.tsx
+++
@@ -1,8 +0,0 @@
-//이름이 [프로젝트 이름]으로 추후 수정
-
-export default function detail(){
-  return (
-    <div>
-    </div>
-  )
-}
