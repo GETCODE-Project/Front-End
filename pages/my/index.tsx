7e5cd569386cdd609fbc5bac9930b97293735961
김한빈 <gksqls0507@gmail.com>
2024-01-05 14:30:50 +0900

    ✨ Feat:  MyPage 페이지 생성

---
+++ pages/my/index.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/common/layout/Footer";
+import Header from "@/components/common/layout/Header";
+import MenuBar from "@/components/common/layout/MenuBar";
+import MyPage from "@/components/myPage/MyPage";
+
+const My = () => {
+    return (
+        <div>
+            <Header />
+            <MenuBar />
+            <MyPage />
+            <Footer />
+        </div>
+    )
+}
+export default My;
