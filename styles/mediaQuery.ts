d8de99e816de5a5cc6a5e4849a1e4878f939f0d8
김한빈 <gksqls0507@gmail.com>
2024-01-05 21:28:09 +0900

    🐛 Fix:  MediaQuery.ts tabletQuery 수정

--- styles/mediaQuery.ts
+++ styles/mediaQuery.ts
@@ -1,11 +1,7 @@
-// const desktopQuery = () => `@media all and (min-width: 700px)`;
-
-const tabletQuery = () => `@media all and (max-width:1140px) and (min-width:601px)`;
-
+const tabletQuery = () => `@media all and (max-width:1140px)`;
 const mobileQuery = () => `@media all and (max-width:600px)`;
 
 export const media = {
-    // desktop: desktopQuery,
     tablet: tabletQuery,
     mobile: mobileQuery,
 };
